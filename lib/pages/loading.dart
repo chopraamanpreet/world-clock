e622c3d8de62eeac6a8ed20cc0b9382fb5ce9687
chopraamanpreet <chopra.aman111@gmail.com>
2020-07-28 00:40:34 +0530

    added datetime and api endpoint for it

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 
 class Loading extends StatefulWidget {
@@ -10,23 +10,31 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async{
+  void getTime() async{
   
-  var url='https://jsonplaceholder.typicode.com/todos/1';
-  var response=await http.get(url);
-
-  print(response.body);
-  print(response.body.runtimeType);
+  var url='http://worldtimeapi.org/api/timezone/Asia/Kolkata';
+  Response response=await get(url);
 
   Map result=jsonDecode(response.body);
-  print(result['title']);
+  String datetime=result['datetime'];
+  String offset=result['utc_offset'].substring(1,3);
+
+  print(datetime);
+  print(offset);
+
+  //print(result);
+
+  DateTime now= DateTime.parse(datetime);
+  print('now $now');
+  now=now.add(Duration(hours:int.parse(offset)));
+  print('Current Time $now');
 }
 
 @override
   void initState() {
     
     super.initState();
-    getData();
+    getTime();
     print("Inside Init state");
   }
   @override
